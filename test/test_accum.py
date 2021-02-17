7d84e5f7b34d15bb717006a480f6bba118caffe8
Jonathan Herring <jonathan.d.herring@gmail.com>
2021-02-16 22:19:24 -0800

    Chapter 6

--- test/test_accum.py
+++ test/test_accum.py
@@ -12,34 +12,44 @@ from stuff.accum import Accumulator
 
 
 # --------------------------------------------------------------------------------------
+# Fixtures
+# --------------------------------------------------------------------------------------
+
+@pytest.fixture
+def accum():
+	yield Accumulator()		# return/yield, yield turns it to a generator
+	print(" ~ Completed ~ ")
+
+
+# --------------------------------------------------------------------------------------
 # Test
 # --------------------------------------------------------------------------------------
 
-def test_accumulator_init():
-	accum = Accumulator()
+def test_accumulator_init(accum):
+	# accum = Accumulator()
 	assert accum.count == 0
 
 
-def test_accumulator_add_one():
-	accum = Accumulator()
+def test_accumulator_add_one(accum):
+	# accum = Accumulator()
 	accum.add()
 	assert accum.count == 1
 
 
-def test_accumulator_add_three():
-	accum = Accumulator()
+def test_accumulator_add_three(accum):
+	# accum = Accumulator()
 	accum.add(3)
 	assert accum.count == 3
 
 
-def test_accumulator_add_twice():
-	accum = Accumulator()
+def test_accumulator_add_twice(accum):
+	# accum = Accumulator()
 	accum.add()
 	accum.add()
 	assert accum.count == 2
 
 
-# def test_accumulator_cannot_set_count_directly():
-# 	accum = Accumulator()
+# def test_accumulator_cannot_set_count_directly(accum):
+# 	# accum = Accumulator()
 # 	with pytest.raises(AttributeError, match=r"can't set attribute") as #<WHAT??!?!?>
 # 		accum.count = 10
