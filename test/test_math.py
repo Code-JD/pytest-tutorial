b39e45235f5db701c3fdd6bd8321ee534630e473
Jonathan Herring <jonathan.d.herring@gmail.com>
2021-02-16 16:20:25 -0800

    Chapter 2

--- test/test_math.py
+++ test/test_math.py
@@ -9,3 +9,13 @@ Their pourpose is to show how to use the pytest framework by expample.
 
 def test_one_plus_one():
 	assert 1 + 1 == 2
+
+# ----------------------------------------------------------------------------------------
+# A test function to show assertion introspection
+# ----------------------------------------------------------------------------------------
+
+def test_one_plus_two():
+	a = 1
+	b = 2
+	c = 3
+	assert a + b == c
