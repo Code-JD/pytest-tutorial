71730cfa90b1a129debc6c751bb378d55fa60568
Jonathan Herring <jonathan.d.herring@gmail.com>
2021-02-16 16:28:13 -0800

    Chapter 3

--- test/test_math.py
+++ test/test_math.py
@@ -1,7 +1,10 @@
+import pytest
+
 """
 This module contains basic unit test for math operations.
 Their pourpose is to show how to use the pytest framework by expample.
 """
+
 
 # ----------------------------------------------------------------------------------------
 # A most basic test function
@@ -9,6 +12,7 @@ Their pourpose is to show how to use the pytest framework by expample.
 
 def test_one_plus_one():
 	assert 1 + 1 == 2
+
 
 # ----------------------------------------------------------------------------------------
 # A test function to show assertion introspection
@@ -19,3 +23,13 @@ def test_one_plus_two():
 	b = 2
 	c = 3
 	assert a + b == c
+
+
+# ----------------------------------------------------------------------------------------
+# A test function that verifies and exception
+# ----------------------------------------------------------------------------------------
+
+def test_divide_by_zero():
+	with pytest.raises(ZeroDivisionError) as e:
+		num = 1 / 0
+	assert 'division by zero' in str(e.value)
