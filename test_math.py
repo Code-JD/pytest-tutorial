ef425ed5e12ad343771f827542fd3b7e1493ea5a
Jonathan Herring <jonathan.d.herring@gmail.com>
2021-02-16 16:06:25 -0800

    second commit, setup check

---
+++ test_math.py
@@ -0,0 +1,11 @@
+"""
+This module contains basic unit test for math operations.
+Their pourpose is to show how to use the pytest framework by expample.
+"""
+
+# ----------------------------------------------------------------------------------------
+# A most basic test function
+# ----------------------------------------------------------------------------------------
+
+def test_one_plus_one():
+	assert 1 + 1 == 2
